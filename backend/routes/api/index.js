c2dfd6416010c1cfefc90643025f1fb0ef0032cf
Joseph Bryant <josephroybryant@gmail.com>
2024-05-15 22:57:41 -0500

    user login testing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
-
 router.use(restoreUser);
 
-// router.post('/test', function(req, res) {
-// res.json({ requestBody: req.body });
-// })
+router.use('/sessions', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', function(req, res) {
+res.json({ requestBody: req.body });
+})
 
 module.exports = router;
