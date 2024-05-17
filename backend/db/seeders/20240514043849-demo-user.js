0f13207ed672b4f5763a5b481f475f5de577402e
Joseph Bryant <josephroybryant@gmail.com>
2024-05-16 22:20:04 -0500

    Finished phase 5

--- backend/db/seeders/20240514043849-demo-user.js
+++ backend/db/seeders/20240514043849-demo-user.js
@@ -15,16 +15,22 @@ module.exports = {
     {
       email: 'demo@user.io',
       username: 'Demo-lition',
+      firstName: 'Mike',
+      lastName: 'Andowsky',
       hashedPassword: bcrypt.hashSync('password')
     },
     {
       email: 'user1@user.io',
       username: 'FakeUser1',
+      firstName: 'Anthony',
+      lastName: 'Weiner',
       hashedPassword: bcrypt.hashSync('password2')
     },
     {
       email: 'user2@user.io',
       username: 'FakeUser2',
+      firstName: 'Judas',
+      lastName: 'Prast',
       hashedPassword: bcrypt.hashSync('password3')
     }
   ], { validate: true })
