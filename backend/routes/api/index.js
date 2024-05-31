d174c767addfb4412a5ffdcd28720fad6173e83d
Joseph Bryant <josephroybryant@gmail.com>
2024-05-31 00:07:07 -0500

    finished spot and review image delete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const { handleValidationErrors } = require('../../utils/validation');
+const { requireAuth } = require('../../utils/auth');
+const { ReviewImage, SpotImage, Spot, Review } = require('../../db/models')
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
-// const spotImagesRouter = require('./spot-images.js');
-// const reviewImagesRouter = require('./review-images.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -20,9 +21,66 @@ router.use('/reviews', reviewsRouter);
 
 router.use('/bookings', bookingsRouter);
 
-// router.use('/spot-images', spotImagesRouter);
+router.delete('/spot-images/:imageId', requireAuth, handleValidationErrors, async (req, res, next) => {
+  try {
+    let imageId = req.params.imageId;
+    // Does image exist
+    let deletedImage = await SpotImage.findByPk(imageId);
+    if (!deletedImage) {
+      res.status(404).json({message: "Spot Image couldn't be found"})
+    }
+    // Does user own image
+    let spotId = deletedImage.spotId;
+    let { user } = req;
+    let spot = await Spot.findOne({
+      where: {
+        id: spotId,
+        ownerId: user.id
+      }
+    })
+    if (!spot) {
+      res.status(400).json({message: "Forbidden"})
+    }
+    // Delete image
+    await deletedImage.destroy();
+    return res.json({ message: 'Successfully deleted' });
+  } catch (error) {
+    next(error)
+  }
+})
 
-// router.use('/review-images', reviewImagesRouter);
+router.delete('/review-images/:imageId', requireAuth, handleValidationErrors, async (req, res, next) => {
+  try {
+    let imageId = req.params.imageId;
+    // Does image exist
+    let deletedImage = await ReviewImage.findByPk(imageId);
+    if (!deletedImage) {
+      res.status(404).json({message: "Review Image couldn't be found"})
+    }
+    // Does user exist
+    let { user } = req;
+    if (!user) {
+      console.log('testtest')
+      res.status(401).json({message: "Authentication required"})
+    }
+    // Does user own image
+    let reviewId = deletedImage.reviewId;
+    let review = await Review.findOne({
+      where: {
+        id: reviewId,
+        userId: user.id
+      }
+    })
+    if (!review) {
+      res.status(400).json({message: "Forbidden"})
+    }
+    // Delete image
+    await deletedImage.destroy();
+    return res.json({ message: 'Successfully deleted' });
+  } catch (error) {
+    next(error)
+  }
+})
 
 router.post('/test', function(req, res) {
 res.json({ requestBody: req.body });
